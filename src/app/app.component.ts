904c177f0a7d73930cc6fe043e9502e63974c5dd
Coffee <tannor.franks@gmail.com>
2020-05-30 14:32:05 -0500

    Surprise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatSidenav} from '@angular/material/sidenav';
 import {NgbDropdown} from '@ng-bootstrap/ng-bootstrap';
 import {Router} from '@angular/router';
+import {SoundMakerService} from './sound-maker.service';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,7 @@ import {Router} from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(public router: Router) {
+  constructor(public router: Router, private soundMakerService: SoundMakerService) {
     this.selectedFriend = this.navLinks.find(x => x.path === this.router.url).label;
     if (this.selectedFriend === 'Home') {
       this.selectedFriend = 'Friends';
@@ -19,6 +20,7 @@ export class AppComponent implements OnInit {
   @ViewChild('NgbDropdown') dropdown: NgbDropdown;
   selectedFriend = 'Friends';
   activeLinkIndex: number;
+  count = 0;
   home = {
     label: 'Home',
     path: '/',
@@ -85,6 +87,26 @@ export class AppComponent implements OnInit {
     this.dropdown.close();
   }
 
+  playIt = () => {
+    this.soundMakerService.playSound('../assets/Fiddlesticks_Original_Attack_2 (online-audio-converter.com).mp3');
+  };
+
+  move() {
+    const btn = document.getElementById('random');
+    if (this.count % 2) {
+      btn.style.top = Math.floor((Math.random() * 1000 + 1)) + 'px';
+      btn.style.left = Math.floor((Math.random() * 1000 + 1)) + 'px';
+    } else {
+      btn.style.top = Math.floor((Math.random() * 256 + 1)) + 'px';
+      btn.style.left = Math.floor((Math.random() * 256 + 1)) + 'px';
+    }
+    this.count++;
+    if (this.count === 5) {
+      this.playIt();
+    }
+    btn.style.fontSize = 17 + (this.count * 10) + 'px';
+    console.log(btn.style.top);
+  }
   // audio = new Audio('../assets/coomer.mp4.mp3');
   // volume = 50;
   // constructor() {
